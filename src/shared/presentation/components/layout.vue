204a318c6b5b8f3ecb8a637b511adb604f1e257e
Sebastian <pepi28z12@gmail.com>
2026-04-20 18:24:01 -0500

    chore: project incomplete.

---
+++ src/shared/presentation/components/layout.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import {Menubar as PvMenubar} from "primevue";
+import LanguageSwitcher from "./language-switcher.vue";
+</script>
+
+<template>
+<div>
+  <pv-menubar>
+    <template #start>
+      <pv-button icon="pi pi-bars" label="CatchUp" text @click="toggleDrawer"/>
+    <---! TODO: Add source list --->
+    </template>
+    <template #end>
+      <language-switcher>
+
+      </language-switcher>
+    </template>
+  </pv-menubar>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
